83375983ea43a28a51374a1f713bfba0b0232944
JohnJ2020 <95947100+JohnJ2020@users.noreply.github.com>
2021-12-10 19:29:36 -0600

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ module.exports = {
     discord: {
         status: 'Gameflip AutoListing',
         statusType: 'PLAYING',
-        token: 'add your discord bot token here lol',
+        token: 'OTE2MTAzMTMxNDgwMzM4NDUz.YalRhg.3Ko87aqo6KZJD3-HfxPqiM1pEf0',
         prefix: '$',
-        ownerIDs: [], // add owner discord id here to grant premium
+        ownerIDs: [], //Vanxh#6969
         ownerOnly: false
     }
 
