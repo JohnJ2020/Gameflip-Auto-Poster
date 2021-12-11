24e781e098e552be1656341ef6258115468a3ad5
JohnJ2020 <95947100+JohnJ2020@users.noreply.github.com>
2021-12-10 19:42:23 -0600

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
         token: 'OTE2MTAzMTMxNDgwMzM4NDUz.YalRhg.3Ko87aqo6KZJD3-HfxPqiM1pEf0',
         prefix: '$',
         ownerIDs: [], //Comic’s Shop⚡134⚡#9242
-        ownerOnly: false
+        ownerOnly: true
     }
 
 }
